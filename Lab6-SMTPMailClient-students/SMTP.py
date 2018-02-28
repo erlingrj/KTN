17a10240d8464d12330bfa1b3eb9e3918dc9eb79
hamsterpoop <erling.jellum@gmail.com>
2018-02-28 11:51:25 +0100

    Add files via upload

--- Lab6-SMTPMailClient-students/SMTP.py
+++ Lab6-SMTPMailClient-students/SMTP.py
@@ -6,22 +6,22 @@ import argparse as ap
 import getpass as gp
 
 #Get sender_email and recipient_email as arguments to the program
-parser = ap.ArgumentParser(description='A test SMTP client without authentication')
-parser.add_argument('-f', '--from', dest='fromMail', required=True, metavar='<sender_email>')
-parser.add_argument('-t', '--to', dest='toMail', required=True, metavar='<recipient_email>')
+#parser = ap.ArgumentParser(description='A test SMTP client without authentication')
+#parser.add_argument('-f', '--from', dest='fromMail', required=True, metavar='<sender_email>')
+#parser.add_argument('-t', '--to', dest='toMail', required=True, metavar='<recipient_email>')
 #If using the authentication of the SMTP server, also get a valid username (optional exercise)
 #parser.add_argument('-u', '--username', dest='username', required=True, metavar='<username>')
 
-args = parser.parse_args()
-fromMail = args.fromMail #Sender's email address
-toMail = args.toMail #Recipient's email address
+#args = parser.parse_args()
+fromMail = "erling@kul.no" #Sender's email address
+toMail = "ingrid@nerd.co" #Recipient's email address
 #username = args.username #SMTP username in case you are implementing the optional exercise
 
 #If using the authentication of the SMTP server, ask for a valid password (optional exercise)
 #password = gp.getpass(prompt='Password: ')
 
 # Message to send
-msg = "\r\n I love computer networks!"
+msg = "\r\n I love sausage"
 endmsg = "\r\n.\r\n"
 
 # Our mail server is smtp.stud.ntnu.no but it allows only authenticated communications. (optional exercise)
@@ -41,7 +41,7 @@ clientSocket.connect((mailserver,PORT))
 #Fill in end
 
 recv = clientSocket.recv(1024)
-print "Recv: ", recv
+print recv
 if recv[:3] != '220':
 	print '220 reply not received from server.'
 
@@ -51,32 +51,37 @@ heloCommand = 'EHLO Hey\r\n'
 #clientSocket.send(heloCommand.encode()) #Python 3
 clientSocket.send(heloCommand) #Python 2.7
 recv1 = clientSocket.recv(1024)
-print "recv1: ", recv1
+print recv1
 if recv1[:3] != '250':
 	print '250 reply not received from server.'
 
 # Send MAIL FROM command and print server response.
-# Fill in start
-# Fill in end
+clientSocket.send('MAIL FROM:erlingerkul@msn.com\r\n') #include \r\n to symbolize end of message
+recv2 = clientSocket.recv(1024)
+print recv2
 
 # Send RCPT TO command and print server response.
-# Fill in start
-# Fill in end
+clientSocket.send('RCPT TO:ingrid@nerd.dk\r\n')
+recv2 = clientSocket.recv(1024)
+print recv2
 
 # Send DATA command and print server response.
-# Fill in start
-# Fill in end
+clientSocket.send('DATA\r\n')
+rcv2 = clientSocket.recv(1024)
+print recv2
 
 # Send message data.
-# Fill in start
-# Fill in end
+clientSocket.send(msg)
+clientSocket.send(endmsg)
+recv2 = clientSocket.recv(1024)
+print recv
 
-# Message ends with a single period.
-# Fill in start
-# Fill in end
+
 
 # Send QUIT command and get server response.
-# Fill in start
-# Fill in end
+clientSocket.send('QUIT\r\n')
+recv2 = clientSocket.recv(1024)
+print recv2
+
 
 #Note that there are more communications if you implement the optional exercise.
